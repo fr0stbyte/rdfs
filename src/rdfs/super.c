e570295671ee7e34d1e39b5ce604819cf864482f
Radu Brumariu <me@fr0stbyte.me>
2016-06-14 23:39:29 -0400

    df works

--- src/rdfs/super.c
+++ src/rdfs/super.c
@@ -1,17 +1,21 @@
 #include<linux/fs.h>
+#include<linux/statfs.h>
 
 #include "rdfs.h"
 #include "internal.h"
 
 static int rdfs_statfs(struct dentry *dir, struct kstatfs *buf) {
+  struct super_block *sb;
+  struct rdfs_superblock *rsb;
   printk(KERN_INFO
          "rdfs_statfs called\n");
 
-  struct super_block *sb = dir->d_sb; // get the superblock
-  struct rdfs_superblock *rsb = (struct rdfs_superblock*)sb->s_fs_info;
+  sb = dir->d_sb; // get the superblock
+  rsb = (struct rdfs_superblock*)sb->s_fs_info;
+
   // start filling in the buf structure
   buf->f_type = RD_MAGIC;
-  buf->f_bsize = RD_BSIZE;
+  buf->f_bsize = sb->s_blocksize;
   buf->f_blocks = RD_MAXBLOCKS;
   buf->f_bfree = rsb->s_nbfree;
   buf->f_bavail = RD_MAXBLOCKS - rsb->s_nbfree;
